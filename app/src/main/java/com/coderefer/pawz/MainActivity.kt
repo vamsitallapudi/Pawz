9c0b6d38fd548ee61ce6b720e072e1c9872f1b41
Vamsi Tallapudi <vamsi@coderefer.com>
2021-03-02 19:26:53 +0530

    refactoring

--- app/src/main/java/com/coderefer/pawz/MainActivity.kt
+++ app/src/main/java/com/coderefer/pawz/MainActivity.kt
@@ -19,41 +19,84 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.coderefer.pawz.ui.theme.MyTheme
-import com.coderefer.pawz.views.PawzMain
+import com.coderefer.pawz.ui.theme.typography
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val isDarkTheme = isSystemInDarkTheme()
-            val toggleTheme = {if (isDarkTheme) setDayTheme() else setDarkTheme()}
-            MyTheme {
-                PawzMain(toggleTheme)
-            }
+//            val isDarkTheme = isSystemInDarkTheme()
+//            val toggleTheme = {if (isDarkTheme) setDayTheme() else setDarkTheme()}
+//            MyTheme {
+//                PawzMain(toggleTheme)
+//            }
+            PawzApp("Vamsi")
         }
     }
 }
 
+//@Preview("Light Theme", widthDp = 360, heightDp = 640)
+//@Composable
+//fun LightPreview() {
+//    MyTheme {
+//        PawzMain {}
+//    }
+//}
+//
+//@Preview("Dark Theme", widthDp = 360, heightDp = 640)
+//@Composable
+//fun DarkPreview() {
+//    MyTheme(darkTheme = true) {
+//        PawzMain {}
+//    }
+//}
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun LightPreview() {
     MyTheme {
-        PawzMain {}
+        PawzApp("Vamsi")
     }
 }
 
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
-fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        PawzMain {}
+fun PawzApp(name:String) {
+    Surface(color = MaterialTheme.colors.background) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.hitler_dog),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(250.dp)
+                    .fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(10.dp)),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.requiredHeight(10.dp))
+            Text(text = "Hello $name Krishna Tallapudi, its the day you born! Thanks for being Awesome and you are the best!",
+                style = typography.h6,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis)
+            Text("Happy Birthday!", style = typography.body2)
+            Text("Have a great day", style = typography.body2)
+        }
     }
 }
 
